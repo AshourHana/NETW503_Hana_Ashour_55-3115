b3e7cf154d23e17e815c154a9d6b725e76bf58ec
AshourHana <147939945+AshourHana@users.noreply.github.com>
2023-12-13 18:32:54 +0200

    Add files via upload

---
+++ MS3_Hana_Ashour_55-3115/Temp2.ino
@@ -0,0 +1,50 @@
+#include <ESP8266WiFi.h>
+#include <ThingsBoard.h>
+
+#ifndef STASSID
+#define STASSID "Salma El Afifi"
+#define STAPSK "123456789"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+const char* thingsboardServer = "demo.thingsboard.io";
+const char* token = "fbq947xxw681qxzylau6"; // Replace with your ThingsBoard access token
+
+WiFiClient espClient;
+ThingsBoard tb(espClient);
+
+const int analogPin = A0;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  tb.connect(thingsboardServer, token);
+}
+
+void loop() {
+  int sensorValue = analogRead(analogPin);
+  float temperatureC = sensorValue; // Adjust the conversion factor as needed
+
+  Serial.print("Temperature: ");
+  Serial.print(temperatureC);
+  Serial.println(" °C");
+
+  tb.sendTelemetryFloat("temperature", temperatureC); // Send temperature to ThingsBoard
+
+  delay(10000);
+}
